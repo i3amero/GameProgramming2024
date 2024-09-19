bb03edc8955934e480e08c6f88b16addf6b775d9
차현준 <92257603+i3amero@users.noreply.github.com>
2024-09-19 15:43:35 +0900

    add HW1

---
+++ Assets/Scripts/Scripts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+        }
+        // s->뒤
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+        }
+    }
+}
